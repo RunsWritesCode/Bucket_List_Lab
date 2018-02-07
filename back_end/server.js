0614ece7ac4c59493547a0662d4b063c916dbf4f
Phil Mitchell <pm1873@gmail.com>
2018-02-07 12:06:45 +0000

    Initial commit

---
+++ back_end/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const server = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mogodb').MongoClient;
+
+server.use(parser.json());
+server.use(express.static('client/build'));
+server.use(parser.urlencoded({extended: true}));
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if (err) {
+    console.log(err);
+    return;
+  };
+
+  const db = client.db("bucket_list");
+
+  server.get('/api/bucketlist', function(req, res) {
+    db.collection("bucket_list").find().toArray(function(err, result) {
+      res.status(200);
+      res.json(result);
+    })
+  });
+
+  server.post('/api/bucketlist', function(req, res) {
+    db.collection("bucket_list").insert(req.body, function(err, result) {
+      if (err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+
+      res.status(201);
+      res.json(result.ops[0]);
+    });
+  });
+
+  server.listen(5000, function() {
+    console.log('Listening on port 5000');
+  });
+
+})
