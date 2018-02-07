9ff489f6b98e462145ee44dcf1ad96e4691467e2
Phil Mitchell <pm1873@gmail.com>
2018-02-07 12:55:56 +0000

    Both servers working

--- back_end/server.js
+++ back_end/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
-const MongoClient = require('mogodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
-server.use(parser.json());
+server.use(bodyParser.json());
 server.use(express.static('client/build'));
-server.use(parser.urlencoded({extended: true}));
+server.use(bodyParser.urlencoded({extended: true}));
 
 MongoClient.connect('mongodb://localhost:27017', function(err, client) {
   if (err) {
